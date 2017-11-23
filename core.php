d9edb6ed555a520e4753c3ae4f80e3517add6817
phambinh217 <phambinh217@gmail.com>
2017-11-23 17:00:45 +0700

    Update

--- core.php
+++ core.php
@@ -7,3 +7,7 @@ include 'config.php';
 
 // Core.php
 // Tệp tin này chứa các thông tin khởi chạy cơ bản như kết nối đến database, session,...
+// Nếu ứng dụng của bạn có kết nối đến database
+// hãy thực hiện kết nối ở đây
+//
+//
